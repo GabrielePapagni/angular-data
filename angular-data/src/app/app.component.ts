6dc8d096920469cd3c39b532d571aa1b2a32fca1
Gabriele Papagni <gabriele.papagni07@gmail.com>
2024-12-09 09:55:05 +0000

    Commit 2

--- angular-data/src/app/app.component.ts
+++ angular-data/src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserItemComponent } from './user-item/user-item.component';
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent, UserItemComponent],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HelloWorldComponent, UserItemComponent, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'angular-data';
 }
